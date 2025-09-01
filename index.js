cbef5127e89533ae9fb691c2e586f53f79a76547
froosterton <jeksonater@gmail.com>
2025-09-01 13:18:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,9 @@ const client = new Client({
     checkUpdate: false,
 });
 
-const MESSAGE_TEXT = '# DM OFFERS';
-const IMAGE_URL = 'https://cdn.discordapp.com/attachments/<id>/<id>/image.png'; // no query params
-
-await channel.send({ content: MESSAGE_TEXT, files: [IMAGE_URL] });
-// or, if you must use a link: await channel.send(`${MESSAGE_TEXT} ${IMAGE_URL}`);
+const MESSAGE_TEXT = '# DM OFFERS @https://media.discordapp.net/attachments/1403173774991560898/1412124048732721203/image.png?ex=68b72681&is=68b5d501&hm=aa3cd0382fdaf7bb6e32b4bd90538fd38362133164b0d7183b5d9dc185f7b350&=&format=webp&quality=lossless&width=523&height=283';
+const CHANNEL_ID = process.env.CHANNEL_ID; // Add this to your .env file
+const INTERVAL = 10 * 60 * 1000 + 30 * 1000; // 10 minutes and 30 seconds in milliseconds
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -34,7 +32,7 @@ client.on('ready', () => {
         try {
             const channel = await client.channels.fetch(CHANNEL_ID);
             if (channel) {
-                await channel.send(MESSAGE);
+                await channel.send(MESSAGE_TEXT);
                 console.log(`Message sent to channel ${CHANNEL_ID}`);
             } else {
                 console.error(`Could not find channel with ID ${CHANNEL_ID}`);
